8797e03e80b70bcdf23acfda1af13e540d52fd89
abidsaleem <abidsaleem@live.com>
2019-03-02 22:30:38 +0300

    Create home, about-us, news home-body and home-header components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './home/home-header/header.component';
 import { BodyComponent } from './body/body.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { NewsComponent } from './news/news.component';
+import { HomeBodyComponent } from './home/home-body/home-body.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,11 @@ import { FooterComponent } from './footer/footer.component';
     NavbarComponent,
     HeaderComponent,
     BodyComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutUsComponent,
+    NewsComponent,
+    HomeBodyComponent
   ],
   imports: [
     BrowserModule,
