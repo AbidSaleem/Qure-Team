ff38b3edca0a90f0fd1813de6a16e3ac4fbe6325
abidsaleem <abidsaleem@live.com>
2019-03-02 22:31:34 +0300

    Create routes for home, about and news

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { NewsComponent } from './news/news.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutUsComponent},
+  {path: 'news', component: NewsComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full' },
+  /* {path: '*', component: ErrorComponent} */
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
