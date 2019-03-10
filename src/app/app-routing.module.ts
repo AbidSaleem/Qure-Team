000e081216f5c90abefb4ec7f0fa79501d7f48ab
abidsaleem <abidsaleem@live.com>
2019-03-10 03:57:13 +0300

    Create Sign-up & Sign-in page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
-import { NewsComponent } from './news/news.component';
+import { NewsComponent } from './pages/news/news.component';
 import { FreeTrialComponent } from './pages/free-trial/free-trial.component';
 import { BlogComponent } from './pages/blog/blog.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { LoginLayoutComponent } from './Login/login-layout/login-layout.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
@@ -14,6 +15,7 @@ const routes: Routes = [
   {path: 'blog', component: BlogComponent},
   {path: 'free-trial', component: FreeTrialComponent},
   {path: 'contact', component: ContactComponent},
+  {path: 'login', component: LoginLayoutComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full' },
   /* {path: '*', component: ErrorComponent} */
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,21 @@ import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
 import { HeaderComponent } from './layout/home-header/header.component';
 
-import { FooterComponent } from './pages/footer/footer.component';
+import { FooterComponent } from './layout/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
-import { NewsComponent } from './news/news.component';
+import { NewsComponent } from './pages/news/news.component';
 import { HomeBodyComponent } from './pages/home/home-body/home-body.component';
 import {BlogComponent} from './pages/blog/blog.component'
 import {FreeTrialComponent} from './pages/free-trial/free-trial.component';
 import { LayoutComponent } from './layout/layout.component';
-import { ContactComponent } from './pages/contact/contact.component'
+import { ContactComponent } from './pages/contact/contact.component';
+import { SignInComponent } from './Login/sign-in/sign-in.component';
+import { SignUpComponent } from './Login/sign-up/sign-up.component';
+import { LoginLayoutComponent } from './Login/login-layout/login-layout.component'
 
 @NgModule({
   declarations: [
@@ -30,7 +33,10 @@ import { ContactComponent } from './pages/contact/contact.component'
     BlogComponent,
     FreeTrialComponent,
     LayoutComponent,
-    ContactComponent
+    ContactComponent,
+    SignInComponent,
+    SignUpComponent,
+    LoginLayoutComponent
   ],
   imports: [
     BrowserModule,
